03566f8869162b8c0c4e40ee39a014c4ab9e3351
yun <lixingyunxing@163.com>
2021-02-27 15:40:59 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_xigua_movie
+
+西瓜电影-flutter
+
+## Getting Started
+
+
+
---
+++ android/app/src/main/kotlin/com/example/flutter_xigua_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_xigua_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
