621cf32499d57e03700f98e1348a45807afa1b38
yun <lixingyunxing@163.com>
2021-03-02 23:50:53 +0800

    no message

--- lib/pages/ranking_list/ranking_content.dart
+++ lib/pages/ranking_list/ranking_content.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_xigua_movie/colors/theme_colors.dart';
 
 class RankingContent extends StatefulWidget {
   final String content;
@@ -11,8 +13,56 @@ class _RankingContentState extends State<RankingContent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text(widget.content),
+      child: ListView.builder(itemBuilder: (bc, index) {
+        return _Item();
+      }),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      margin: EdgeInsets.symmetric(horizontal: 30.w, vertical: 10.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              width: 100,
+              color: ThemeColors.imageBgColor,
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '电影名称',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  '电影名称',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
